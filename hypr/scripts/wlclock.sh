dcb21e9a2951397b30a561577c4a785840f2d343
frort <87010782+kienn-HCl@users.noreply.github.com>
2023-08-27 17:44:58 +0900

    add hyprland confg

---
+++ hypr/scripts/wlclock.sh
@@ -0,0 +1,2 @@
+#!/bin/env bash
+wlclock --layer bottom --position bottom-right --size 400 --margin 50 --corner-radius 200 --background-colour '#282828' --clock-colour '#B8BB26'
---
+++ hypr/scripts/wlrSunclock.sh
@@ -0,0 +1,2 @@
+#!/bin/env bash
+wlr-sunclock --margins '0,16,16,0' --width 1200 --layer bottom --anchors tr --border-width 2 --colour-ocean '#282828' --colour-land '#B8BB26'
