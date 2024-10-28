1701c06002ad7b2a553b4b45a14f4267e8a6d745
kienn-HCl <87010782+kienn-HCl@users.noreply.github.com>
2024-10-29 01:34:32 +0900

    sway & rofi: sway's rofi bind command change. And fix indent.

--- rofi.nix
+++ rofi.nix
@@ -1,4 +1,4 @@
 { pkgs, ...}: {
-    home.packages = [ pkgs.rofi-wayland ];
-    xdg.configFile."rofi".source = ./rofi ;
+  home.packages = [ pkgs.rofi-wayland ];
+  xdg.configFile."rofi".source = ./rofi ;
 }
