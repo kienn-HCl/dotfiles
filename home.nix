8b0c7fb45ab8e3d5a663394426f21ff83bd60ced
kienn-HCl <87010782+kienn-HCl@users.noreply.github.com>
2024-10-31 21:34:43 +0900

    home.nix: add gnuplot

--- home.nix
+++ home.nix
@@ -54,6 +54,8 @@ rec {
     zulip
     spotify
 
+    gnuplot
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
