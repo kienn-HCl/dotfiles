1aa0a27da5bd8d53069e31b4630b4da5690d61b8
kienn-HCl <87010782+kienn-HCl@users.noreply.github.com>
2025-01-25 18:34:04 +0900

    add some app

--- home.nix
+++ home.nix
@@ -67,10 +67,15 @@ rec {
     zathura
     nwg-displays
     nur.repos.zzzsy.zen-browser
-    (config.lib.nixGL.wrap pkgs.bambu-studio)
+    # (config.lib.nixGL.wrap pkgs.bambu-studio)
     (config.lib.nixGL.wrap pkgs.openscad-unstable)
+    (config.lib.nixGL.wrap pkgs.freecad-wayland)
+    (config.lib.nixGL.wrap pkgs.steam)
 
     xwayland-satellite
+
+    wineWowPackages.waylandFull
+    winetricks
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
