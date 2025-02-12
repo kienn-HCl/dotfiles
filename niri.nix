3effcf3b7f7a5eb356f0bec5a795ee432c0c120b
kienn-HCl <87010782+kienn-HCl@users.noreply.github.com>
2025-02-13 02:56:10 +0900

    add niri setting file & concat splited config file with nix

--- niri.nix
+++ niri.nix
@@ -1,24 +1,35 @@
 { pkgs, config, ... }:
+let
+  kdlFiles = pkgs.lib.filesystem.listFilesRecursive ./niri;
+  filteredKdlFiles = builtins.filter (f: pkgs.lib.strings.hasSuffix ".kdl" (toString f)) kdlFiles;
+  congigFile = builtins.concatStringsSep "\n" (map builtins.readFile filteredKdlFiles);
+in
 {
+  xdg.configFile = {
+    "niri/config.kdl" = {
+      text = congigFile;
+      recursive = true;
+    };
+  };
   home.packages = [
-    (config.lib.nixGL.wrap pkgs.niri)
-    (pkgs.writeShellScriptBin "set-niri-session" ''
-      case $1 in
-          add)
-              sudo cp ${pkgs.niri}/share/wayland-sessions/niri.desktop /usr/share/wayland-sessions/
-              ;;
-          delete)
-              sudo rm /usr/share/wayland-sessions/niri.desktop
-              ;;
-          *)
-              echo "Usage: $(basename $0) (add|delete)"
-              exit 1
-              ;;
-      esac
-    '')
+    # (config.lib.nixGL.wrap pkgs.niri)
+    # (pkgs.writeShellScriptBin "set-niri-session" ''
+    #   case $1 in
+    #       add)
+    #           sudo cp ${pkgs.niri}/share/wayland-sessions/niri.desktop /usr/share/wayland-sessions/
+    #           ;;
+    #       delete)
+    #           sudo rm /usr/share/wayland-sessions/niri.desktop
+    #           ;;
+    #       *)
+    #           echo "Usage: $(basename $0) (add|delete)"
+    #           exit 1
+    #           ;;
+    #   esac
+    # '')
   ];
   home.file = {
-    ".local/share/systemd/user/niri-shutdown.target".source = "${pkgs.niri}/share/systemd/user/niri-shutdown.target";
-    ".local/share/systemd/user/niri.service".source = "${pkgs.niri}/share/systemd/user/niri.service";
+    # ".local/share/systemd/user/niri-shutdown.target".source = "${pkgs.niri}/share/systemd/user/niri-shutdown.target";
+    # ".local/share/systemd/user/niri.service".source = "${pkgs.niri}/share/systemd/user/niri.service";
   };
 }
