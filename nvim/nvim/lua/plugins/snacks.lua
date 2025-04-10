7ec3f13b1725c1aa1be6392d15fac4860ab45688
kienn-HCl <87010782+kienn-HCl@users.noreply.github.com>
2025-04-10 23:49:21 +0900

    add snacks

---
+++ nvim/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      explorer = { enabled = true },
+      picker = { enabled = true },
+      notifier = { enabled = true },
+      scope = { enabled = true },
+      scroll = { enabled = true },
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
+    },
+    keys = {
+      { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
+      { "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
+      { "<leader>b", function() Snacks.picker.buffers() end, desc = "Buffers" },
+    },
+  },
+}
