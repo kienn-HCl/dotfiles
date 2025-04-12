efe05897a67919c74c15d4057c386891ccf2399a
kienn-HCl <87010782+kienn-HCl@users.noreply.github.com>
2025-04-12 19:18:28 +0900

    add bufdelete

--- nvim/nvim/lua/plugins/snacks.lua
+++ nvim/nvim/lua/plugins/snacks.lua
@@ -8,11 +8,11 @@ return {
       -- your configuration comes here
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
+      bufdelete = { enabled = true },
       explorer = { enabled = true },
       picker = { enabled = true },
       notifier = { enabled = true },
       scope = { enabled = true },
-      scroll = { enabled = true },
       statuscolumn = { enabled = true },
       words = { enabled = true },
     },
