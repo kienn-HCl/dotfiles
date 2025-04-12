6b703d09ab5a2e244a58c5f72bde9f47c2989d09
kienn-HCl <87010782+kienn-HCl@users.noreply.github.com>
2025-04-12 19:18:02 +0900

    add hlchunk

---
+++ nvim/nvim/lua/plugins/hlchnk.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "shellRaining/hlchunk.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("hlchunk").setup({
+        chunk = {
+          enable = true,
+          duration = 0,
+          delay = 0
+        },
+      })
+    end
+  },
+}
