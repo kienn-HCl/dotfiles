466750fdb7d21c7d1b3d11cc6f26ed8a818649b8
frort <87010782+kienn-HCl@users.noreply.github.com>
2025-04-15 19:03:46 +0900

    add new plugin

---
+++ nvim/nvim/lua/plugins/nvim-anydent.lua
@@ -0,0 +1,13 @@
+return {
+  "hrsh7th/nvim-anydent",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function()
+        if not vim.tbl_contains({ "html", "yaml", "markdown" }, vim.bo.filetype) then
+          require("anydent").attach()
+        end
+      end,
+    })
+  end,
+}
