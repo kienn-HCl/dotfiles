3d47cf3df7c9bde5fbf2cbd90eac85e2f450b770
frort <87010782+kienn-HCl@users.noreply.github.com>
2025-04-17 14:19:19 +0900

    補完まわりのkeymapの挙動を好みに合うよう改善

--- nvim/nvim/lua/plugins/completion.lua
+++ nvim/nvim/lua/plugins/completion.lua
@@ -12,6 +12,7 @@ return {
       "hrsh7th/cmp-nvim-lsp-signature-help",
     },
     config = function()
+      local luasnip = require("luasnip")
       local cmp = require("cmp")
       cmp.setup({
         snippet = {
@@ -24,13 +25,48 @@ return {
           documentation = cmp.config.window.bordered(),
         },
         mapping = cmp.mapping.preset.insert({
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-u>"] = cmp.mapping.scroll_docs(4),
           ["<C-l>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ["<C-p>"] = cmp.mapping.select_prev_item(),
-          ["<C-n>"] = cmp.mapping.select_next_item(),
+          -- ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          -- ["<C-p>"] = cmp.mapping.select_prev_item(),
+          -- ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<CR>"] = cmp.mapping({
+            i = function(fallback)
+              if cmp.visible() and cmp.get_active_entry() then
+                if luasnip.expandable() then
+                  luasnip.expand()
+                else
+                  cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+                end
+              else
+                fallback()
+              end
+            end,
+            s = cmp.mapping.confirm({ select = true }),
+            -- c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+          }),
+
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.locally_jumpable(1) then
+              luasnip.jump(1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
@@ -52,7 +88,14 @@ return {
 
       -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
       cmp.setup.cmdline(":", {
-        mapping = cmp.mapping.preset.cmdline(),
+        mapping = cmp.mapping.preset.cmdline({
+          ["<C-p>"] = function(fallback)
+            fallback()
+          end,
+          ["<C-n>"] = function(fallback)
+            fallback()
+          end,
+        }),
         sources = cmp.config.sources({
           { name = "path" },
         }, {
