808ed05f7d99c1ba3caff28c1254d73f0796a2c9
kienn-HCl <87010782+kienn-HCl@users.noreply.github.com>
2025-04-24 20:09:25 +0900

    rcp abbrを追加

--- zsh/default.nix
+++ zsh/default.nix
@@ -39,6 +39,7 @@
         gst = "git status";
         gcm = "git commit -m \"%\"";
         gd = "git diff";
+        rcp = "rsync -ah --no-i-r --info=progress2";
       };
     };
     syntaxHighlighting = {
