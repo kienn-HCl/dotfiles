56df4f45665143c096c29b6fa7761dce9f7ead2e
kienn-HCl <87010782+kienn-HCl@users.noreply.github.com>
2025-05-19 14:50:50 +0900

    ホスト名で適用する設定ファイルを変えるよう変更

--- niri/default.nix
+++ niri/default.nix
@@ -2,7 +2,12 @@
 let
   kdlFiles = pkgs.lib.filesystem.listFilesRecursive ./niri;
   filteredKdlFiles = builtins.filter (f: pkgs.lib.strings.hasSuffix ".kdl" (toString f)) kdlFiles;
-  congigFile = builtins.concatStringsSep "\n" (map builtins.readFile filteredKdlFiles);
+
+  hostname = /etc/hostname |> builtins.readFile |> builtins.replaceStrings ["\n"] [""];
+  hostDepDir = if builtins.pathExists ./niri-${hostname} then ./niri-${hostname} else ./niri-common;
+  hostDepFiles = pkgs.lib.filesystem.listFilesRecursive hostDepDir;
+  filteredHostFiles = builtins.filter (f: pkgs.lib.strings.hasSuffix ".kdl" (toString f)) hostDepFiles;
+  congigFile = builtins.concatStringsSep "\n" (map builtins.readFile (filteredKdlFiles ++ filteredHostFiles));
 in
 {
   xdg.configFile = {
