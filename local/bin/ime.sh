dc6cdb559ba044e34b04ecf191d5f821dad0c447
kienn-HCl <87010782+kienn-HCl@users.noreply.github.com>
2025-06-19 18:56:39 +0900

    文字入力時の末尾改行二対応

--- local/bin/ime.sh
+++ local/bin/ime.sh
@@ -2,7 +2,8 @@
 
 foot -T "vim ime" nvim /tmp/ime || exit 1
 if [[ -e /tmp/ime ]]; then
-    wl-copy -n < /tmp/ime
+    echo -n "$(< /tmp/ime)" | wtype -
+    foot sleep 0.001
     notify-send -t 1000 copied
     rm -f /tmp/ime
 fi
--- local/bin/select-paste.sh
+++ local/bin/select-paste.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -eo pipefail
-cliphist list | rofi -dmenu | cliphist decode | wl-copy && wl-paste -n | wtype -
+cliphist list | rofi -dmenu | cliphist decode | wl-copy && echo -n "$(wl-paste -n)" | wtype -
 foot sleep 0.001
