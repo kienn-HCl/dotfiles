e9cd38c32a367437886c5ead8b15624b89b93f4d
kienn-HCl <87010782+kienn-HCl@users.noreply.github.com>
2025-12-30 01:18:40 +0900

    fix

--- mpv/default.nix
+++ mpv/default.nix
@@ -1,9 +1,9 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   programs.mpv = {
     enable = true;
     package = pkgs.mpv-unwrapped.wrapper {
-      mpv = (pkgs.mpv-unwrapped.override { sixelSupport = true; });
+      mpv = (config.lib.nixGL.wrap (pkgs.mpv-unwrapped.override { sixelSupport = true; }));
     };
   };
 }
